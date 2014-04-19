a195788df58a3b168c615455d05b109eeac4bcb1
Venkat Palivela <venkat.palivela@gmail.com>
2014-04-19 18:44:57 -0500

    Added Http Connection

---
+++ TitanIOS/TitanIOS/HttpConnection.m
@@ -0,0 +1,18 @@
+//
+//  HttpConnection.m
+//  TitanIOS
+//
+//  Created by venkat on 4/19/14.
+//  Copyright (c) 2014 Venkat Palivela. All rights reserved.
+//
+
+#import "HttpConnection.h"
+
+@interface HttpConnection ()
+
+
+@end
+
+@implementation HttpConnection
+
+@end
